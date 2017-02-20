cd0572a70b8672610d2723d724d0c3ed5c0b4e16
chenshengMain <2572157262@qq.com>
2017-02-20 22:23:43 +0800

    XcodeFirstCommit

---
+++ MyXcodeObject/MyXcodeObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyXcodeObject
+//
+//  Created by 陈胜 on 2017/2/20.
+//  Copyright © 2017年 陈胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyXcodeObject/MyXcodeObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyXcodeObject
+//
+//  Created by 陈胜 on 2017/2/20.
+//  Copyright © 2017年 陈胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
